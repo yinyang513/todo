67e2ec7c5a591646fe0005bcdfa25159def52e2a
yinyang513 <zyingyip@gmail.com>
2021-08-09 22:43:25 -0400

    delete comments

--- app.py
+++ app.py
@@ -16,52 +16,22 @@ def home():
 def create_note():
     note = request.args.get('note')
     notes.insert({'note': note})
-    allTodos = todos.find()
-    allNotes = notes.find()
-    for todo in allTodos:
-        print(todo)
-    print()
-    for notea in allNotes:
-        print(notea)
     return note
 
 @app.route('/add-todo',methods=['POST'])
 def add_todo():
     todo = request.args.get('todo')
     note = request.args.get('note')
-    # notess = notes.find()
-    # for note in notess:
-    #     print(note)
 
     find_note = notes.find_one({'note': note})
-    # print(find_note)
     if find_note:
         todos.insert({'note': note,'todo': todo, 'complete': False})
-    allTodos = todos.find()
-    allNotes = notes.find()
-    for todo in allTodos:
-        print(todo)
-    print()
-    for note in allNotes:
-        print(note)
-    # print(lol)
     return 'inserted note'
 
 @app.route('/complete-todo',methods=['POST'])
 def complete_todo():
-    complete = request.args.get('complete') # boolean
     todo = request.args.get('todo')
-    
-    temp = todos.update({'todo': todo}, {'$set': {'complete': complete}})
-    # temp['complete'] = complete
-    print(temp)
-    allTodos = todos.find()
-    allNotes = notes.find()
-    for todo in allTodos:
-        print(todo)
-    print()
-    for note in allNotes:
-        print(note)
+    temp = todos.update({'todo': todo}, {'$set': {'complete': True}})
     return 'completed'
 
 @app.route('/get-all-todos',methods=['GET'])
@@ -71,9 +41,9 @@ def get_todos():
     for todo in all_todos:
         ret.append({
             'note': todo['note'],
-            'todo': todo['todo']
+            'todo': todo['todo'],
+            'completed': todo['complete']
         })
-    print(ret)
     return jsonify(ret)
 
 @app.route('/delete',methods=['POST'])
@@ -85,13 +55,6 @@ def delete_todos():
     if note:
         notes.remove({'note': note})
         todos.remove({'note': note})
-    allTodos = todos.find()
-    allNotes = notes.find()
-    for todo in allTodos:
-        print(todo)
-    print()
-    for note in allNotes:
-        print(note)
     return 'delete'
 
 if __name__ == "__main__": 
